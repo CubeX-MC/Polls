0777f94e1c38698bd3ffbae58c6559afe8bb2b0e
FZAoao <785971038@qq.com>
2026-07-14 06:22:37 -0400

    插件启动时检测并初始化平台适配器

--- src/main/java/com/polls/PollsPlugin.java
+++ src/main/java/com/polls/PollsPlugin.java
@@ -3,6 +3,9 @@ package com.polls;
 import com.polls.db.Database;
 import com.polls.db.PollCache;
 import com.polls.gui.MainGui;
+import com.polls.platform.PlatformAdapter;
+import com.polls.platform.PaperAdapter;
+import com.polls.platform.BukkitAdapter;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -15,11 +18,22 @@ public class PollsPlugin extends JavaPlugin implements CommandExecutor {
     private Database database;
     private PollCache pollCache;
     private PollScheduler scheduler;
+    private PlatformAdapter platformAdapter;
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
         getDataFolder().mkdirs();
+
+        // 检测并初始化平台适配器
+        try {
+            Class.forName("io.papermc.paper.threadedregions.scheduler.AsyncScheduler");
+            platformAdapter = new PaperAdapter(this);
+            getLogger().info("检测到 Paper 服务器，启用 Adventure API 支持");
+        } catch (ClassNotFoundException e) {
+            platformAdapter = new BukkitAdapter(this);
+            getLogger().info("使用 Bukkit/Spigot/Folia 兼容模式");
+        }
 
         database = new Database(this);
         try {
@@ -64,4 +78,5 @@ public class PollsPlugin extends JavaPlugin implements CommandExecutor {
     public String getAdminPermission() {
         return getConfig().getString("admin-permission", "polls.admin");
     }
+    public PlatformAdapter getPlatformAdapter() { return platformAdapter; }
 }
